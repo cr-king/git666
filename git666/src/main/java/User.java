40058529f75eb04e17ad393698e67b7cdbb8c238
CR <543014543@qq.com>
2019-06-12 18:58:23 +0800

    dcfc

--- git666/src/main/java/User.java
+++ git666/src/main/java/User.java
@@ -1,4 +1,12 @@
 public class User {
     private String name;
     private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
