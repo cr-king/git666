4c5bb9379ef5eeb416c5a4dd66871fa2412c2bbb
CR <543014543@qq.com>
2019-06-12 20:35:46 +0800

    搭档

--- git666/src/main/java/User.java
+++ git666/src/main/java/User.java
@@ -2,11 +2,5 @@ public class User {
     private String name;
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
